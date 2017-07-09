284e4ee7c4c37a9e65fd4cb769d863455d5f3cfc
C. David Byrd <db@gatech.edu>
2017-07-09 17:17:02 -0400

    Added new localized English messages for all note types (asset/issue).

--- LocalizedMessages.swift
+++ LocalizedMessages.swift
@@ -21,4 +21,23 @@ class LocalizedMessages: NSObject {
     static let TRIP_ERRAND = "The primary reason for this bike trip is to attend to personal business such as banking, doctor visit, going to the gym, etc."
     static let TRIP_OTHER = "If none of the other reasons apply to this trip, you can enter trip comments after saving your trip to tell us more."
     
+    
+    // Messages for noting issues.
+    static let NOTE_ISSUE_REPAIR = "Here's a spot where the road needs to be repaired (pothole, rough concrete, gravel in the road, manhole cover, sewer grate)."
+    static let NOTE_ISSUE_SIGNAL = "Here's a signal that you can't activate with your bike."
+    static let NOTE_ISSUE_ENFORCEMENT = "The bike lane is always blocked here, cars disobey \"no right on red\", or anything where the cops can help make cycling safer."
+    static let NOTE_ISSUE_PARKING = "You need a bike rack to secure your bike here."
+    static let NOTE_ISSUE_LANE = "Where the bike lane ends (abruptly) or is too narrow (pesky parked cars)."
+    static let NOTE_ISSUE_OTHER = "Anything else ripe for improvement: want a sharrow, a sign, a bike lane? Share the details."
+    
+    // Messages for noting assets.
+    static let NOTE_ASSET_PARKING = "Park them here and remember to secure your bike well! Please only include racks or other objects intended for bikes."
+    static let NOTE_ASSET_BIKE_SHOPS = "Have a flat, a broken chain, or spongy brakes? Or do you need a bike to jump into this world of cycling in the first place? Here's a shop ready to help."
+    static let NOTE_ASSET_WASH_UP = "Help us make cycling mainstream. Here's a place to refresh yourself before you re-enter the fashionable world of Atlanta."
+    static let NOTE_ASSET_SHORT_CUT = "Here's an access point under the tracks, through the park, onto a trail, or over a ravine."
+    static let NOTE_ASSET_WATER = "Here's a spot to fill your bottle on those hot summer days. Stay hydrated, people. We need you."
+    static let NOTE_ASSET_OTHER = "Anything else we should map to help your fellow cyclists? Share the details."
+    
+
+    
 }
